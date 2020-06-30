3441f699afff0ada81705941394d3e40ff824356
Ferdo Vukojevic <f.vukojevic@globalsoft.ba>
2020-06-30 12:49:37 +0200

    humanize, listings view and controller logic

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+from .models import Listing
+
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    page_listings = paginator.get_page(page)
+    context = {
+        'listings': page_listings,
+    }
+    return render(request, 'listings/listings.html', context)
+
 
 def listing(request, listing_id):
     return render(request, 'listings/listing.html')
+
 
 def search(request):
     return render(request, 'listings/search.html')
